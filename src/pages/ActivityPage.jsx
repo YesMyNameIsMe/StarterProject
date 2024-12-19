16cc8211a7f14fc174b767def68fd8a0f13fb510
YesMyNameIsMe <nguyen424787@gmail.com>
2024-12-18 19:12:09 -0600

    Vote Component

--- src/pages/ActivityPage.jsx
+++ src/pages/ActivityPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import VoteComponent from '../components/VoteComponent'
 
 function ActivityPage() {
   return (
-    <div>Activity Page</div>
+    <div>
+      <div className="relative my-12 flex flex-col items-center">
+        Voting
+        <div className="relative z-10 p-6 w-full max-w-2x1 flex flex-col gap-6">
+          <VoteComponent />
+        </div>
+      </div>
+    </div>
   );
 }
 
